3c3d945cab1a1b95ecd8f287fc9ea84ffde4a421
alexandryudaev <akayuki@yandex.ru>
2021-04-06 00:28:11 +0300

    getUserInfo function

--- index.js
+++ index.js
@@ -2,45 +2,51 @@ const puppeteer = require('puppeteer');
 const fs = require("fs");
 
 let config = JSON.parse(fs.readFileSync('config.json'));
-let userData = {};
 
 auth = async (page, config) => {
-    await page.goto(config.url.main + config.url.auth);
+    if (await page.url().split('?')[0] !== (config.url.main + config.url.auth))
+        await page.goto(config.url.main + config.url.auth);
     await page.$eval('input[name=user]', (el, username) => el.value = username, config.username);
     await page.$eval('input[name=pass]', (el, password) => el.value = password, config.password);
     await page.click('[type="submit"]');
 }
 
+getUserInfo = async (page, config) => {
+    let data = {};
+    let currentUrl = await page.url().split('?')[0];
+
+    if (currentUrl !== (config.url.main + config.url.profile)) await page.goto(config.url.main);
+    await page.waitForSelector('.gold');
+
+    let userInfo = await page.$eval('.gold',el =>
+        el
+            .textContent
+            .trim()
+            .split('\n')
+            .map(el => el.trim())
+    );
+    data.gold = userInfo[0].replace('.','');
+    data.energy = userInfo[3].match(/^\d+/)[0];
+    data.hp = userInfo[4].replace('.','').trim().match(/^\d+/)[0];
+    data.power = userInfo[5].split(' ').map(el => el.trim());
+    data.power = data.power[data.power.length - 1];
+
+    return data;
+}
+
 start = async (config) => {
     const browser = await puppeteer.launch({
-        headless: false, // The browser is visible
+        headless: false,
         ignoreHTTPSErrors: true,
         defaultViewport: null,
-        args: [`--window-size=${config.width},${config.height}`] // new option
+        args: [`--window-size=${config.width},${config.height}`]
     });
     const page = await browser.newPage();
     await page.goto(config.url.main);
     if (await page.$('.cookiebanner1') !== null) await page.click('[class="cookiebanner5"]');
     if (await page.$('#regBtn') !== null) await auth(page, config);
-    await page.waitForSelector('.gold');
-    let userInfo = await page.$eval('.gold',el =>
-        el
-            .textContent
-            .trim()
-            //.split('&nbsp;')
-            .split('\n')
-            .map(el => el.trim())
-    );
-    userData.gold = userInfo[0];
-    userData.energy = userInfo[3];
-    userData.hp = userInfo[4];
-    userData.power = userInfo[5].split(' ').map(el => el.trim());
-    userData.power = userData.power[userData.power.length - 1];
+    let userData = await getUserInfo(page, config);
 
-    // let nameLengths = names.map(function(name) {
-    //     return name.length;
-    // });
-    // console.log(value[5].split(' ').map(el => el.trim()));
     console.log(userData);
 };
 
