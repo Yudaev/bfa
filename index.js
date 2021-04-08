8cebb46185235ba29d232ab78b7c31d833695eab
alexandryudaev <akayuki@yandex.ru>
2021-04-08 14:43:11 +0300

    hunting cycle

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const puppeteer = require('puppeteer');
 const fs = require("fs");
 
-let config = JSON.parse(fs.readFileSync('config.json'));
+let config = JSON.parse(fs.readFileSync('config.json')),
+    healingCount = null;
 
 auth = async (browser, page, config) => {
     if (await page.url().split('?')[0] !== (config.url.main + config.url.auth))
@@ -11,10 +12,10 @@ auth = async (browser, page, config) => {
     await page.click('[type="submit"]');
     try{
         await page.waitForSelector('#infobar');
-        console.log('Successful login by', config.username);
+        console.log(`[${logTime()}] Successful login by ${config.username}`);
     }catch (e) {
         browser.close();
-        throw new Error('Failed login by ' + config.username);
+        throw new Error('[' + logTime() + ']' + 'Failed login by ' + config.username);
     }
 }
 
@@ -48,41 +49,47 @@ getUserInfo = async (page, config) => {
     return data;
 }
 
-hunting = async (page, config) => {
-    console.log('I start hunting');
-    let userData;
+hunting = async (page, config, userData) => {
+    let currentUrl = await page.url().split('?')[0];
+    if (currentUrl !== config.url.main + config.url.hunt) await page.goto(config.url.main + config.url.hunt);
+    let enemyPower = Math.round(userData.power - userData.power * 0.02);
 
-    do {
-        let currentUrl = await page.url().split('?')[0];
-        if (currentUrl !== config.url.main + config.url.hunt) await page.goto(config.url.main + config.url.hunt);
-        userData = await getUserInfo(page, config);
+    await page.$eval('[name="lvlbis"]', (el, enemyPower) => el.value = enemyPower, enemyPower);
+    await page.click('[name="levelsearch"]');
+    await page.waitForSelector('.cost');
+    await page.click('.cost');
 
-        let enemyPower = Math.round(userData.power - userData.power * 0.02);
+    await page.waitForSelector('#fighter_details')
+    let enemyName = await page.$eval('#fighter_details_defender h3 a',el => el.textContent);
+    let enemyGold = await page.$eval('p.gold',el => {
+            let allNumbers = el
+                .textContent
+                .trim()
+                .replace(/(\s\r\n|\n|\r|\s)/gm, "")
+                .replace('.', '')
+                .replace('+', '')
+                .match(/\d+/g);
+            return +allNumbers[0] + +allNumbers[1];
+        }
+    );
+    let winner = await page.$eval('.wrap-content > h3', el => el.textContent);
+    console.log(`[${logTime()}] ${winner}. Attacked ${enemyName}. My HP at start: ${userData.hp}. Gold: ${enemyGold}`);
 
-        await page.$eval('[name="lvlbis"]', (el, enemyPower) => el.value = enemyPower, enemyPower);
-        await page.click('[name="levelsearch"]');
-        await page.waitForSelector('.cost');
-        await page.click('.cost');
-
-        await page.waitForSelector('#fighter_details')
-        let enemyName = await page.$eval('#fighter_details_defender h3 a',el => el.textContent);
-        let enemyGold = await page.$eval('p.gold',el => {
-                let allNumbers = el
-                    .textContent
-                    .trim()
-                    .replace(/(\s\r\n|\n|\r|\s)/gm, "")
-                    .replace('.', '')
-                    .replace('+', '')
-                    .match(/\d+/g);
-                return +allNumbers[0] + +allNumbers[1];
-            }
-        );
-        let winner = await page.$eval('.wrap-content > h3', el => el.textContent);
-        console.log(winner, '. Attacked', enemyName, '. My HP:', userData.hp, '. Gold:', enemyGold)
-    } while (userData.hp > config.userHPmin);
-
-    console.log('User data after the hunt', await getUserInfo(page, config))
+    return await getUserInfo(page, config);
 }
+
+logTime = () => {
+    let currentdate = new Date();
+    let datetime = currentdate.getDate() + '.'
+        + (currentdate.getMonth() + 1)  + '.'
+        + currentdate.getFullYear() + ' '
+        + currentdate.getHours() + ':'
+        + currentdate.getMinutes() + ':'
+        + (currentdate.getSeconds() < 10 ? '0' + currentdate.getSeconds() : currentdate.getSeconds());
+    return datetime;
+}
+
+sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
 
 start = async (config) => {
     const browser = await puppeteer.launch({
@@ -95,8 +102,19 @@ start = async (config) => {
     await page.goto(config.url.main);
     if (await page.$('.cookiebanner1') !== null) await page.click('[class="cookiebanner5"]');
     if (await page.$('#regBtn') !== null) await auth(browser, page, config);
-    await hunting(page, config);
 
+    console.log(`[${logTime()}] I start hunting`);
+    let userData = await getUserInfo(page, config);
+    while (userData.hp > config.userHPmin) userData = await hunting(page, config, userData);
+    console.log('[' + logTime() + ']', 'User data after the hunt', await getUserInfo(page, config));
+
+    // if (await page.url().split('?')[0] !== (config.url.main + config.url.profile))
+    //     await page.goto(config.url.main + config.url.profile);
+    // healingCount = await page.$('span#healing_countdown') !== null ?
+    //     await page.$eval('span#healing_countdown',el => el.textContent) : null;
+    // console.log(healingCount)
+
+    //console.log(await page.$('span#healing_countdown'))
     await browser.close();
 };
 
